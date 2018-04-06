79821fbdc8104800b37ebb29c45e74897b5b18f0
Dawnabelle <dawn.mott@gmail.com>
2018-04-06 09:44:03 -0700

    add business logic to js, change title on index.html

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// business logic
+
+// Pizza object constructor w properties size, toppings
+function Pizza(size, veggies) {
+  this.size = size;
+  this.veggies = veggies;
+}
+
+// prototype method cost pizza depending on toppings
+Pizza.prototype.priceCost = function() {
+  var veggiePrice = (this.veggies.length * 1);
+
+// conditional statement size cost
+  if (this.size === 'tiny'){
+    var price = 5;
+  } else if (this.size === 'medium'){
+    var price = 12;
+  } else if (this.size === 'large'){
+    var price = 17;
+  } else if (this.size === 'gigantor'){
+    var price = 23;
+  }
+
+// calculate final cost
+  var pizzaPrice = price + veggiePrice;
+  return pizzaPrice;
+}
